0d034f09ab20eec60d4e709b0004d316fe77572a
excess30 <sejal_100@hotmail.com>
2020-03-21 12:32:19 +0530

    Only For Testing Text Box Needs To Be Done.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import ImageUploader from './component/index.js';
+export default class App extends React.PureComponent {
+    render() {
+        return (
+            <div className="page">
+                <h1>Please Upload The Image </h1>
+                <p>Demonstration Of An End To End System For The Detection Of Brain Tumor </p>
+                <div className="head">Demo</div>
+                <ImageUploader style={{ maxWidth: '500px', margin: "20px auto" }}
+                               withPreview={true} /> 
+                <ImageUploader style={{ maxWidth: '500px', margin: "20px auto" }}
+                                withPreview={true} /> 
+                <ImageUploader style={{ maxWidth: '500px', margin: "20px auto" }}
+                                withPreview={true} /> 
+                <ImageUploader style={{ maxWidth: '500px', margin: "20px auto" }}
+                                withPreview={true} /> 
+            </div>
+        );
+    }
 }
-
-export default App;
